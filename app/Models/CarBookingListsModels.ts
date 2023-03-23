7df97479a3e7e523bf1aac91d48bfafd48e0bb90
siwit <siwit@hotmail.co.th>
2023-03-23 18:08:31 +0700

    update model bookinglist

--- app/Models/CarBookingListsModels.ts
+++ app/Models/CarBookingListsModels.ts
@@ -1,22 +1,78 @@
-//import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
 
 export default class CarBookingModel extends BaseModel {
 
   public static table = 'car_booking'
   @column({ isPrimary: true })
-  public booking_id: string
+  public booking_id: number
 
   @column({ isPrimary: true })
   public hospcode: string
 
-  @column()
-  public startdate: Date
-  @column()
-  public starttime: Date
-  @column()
-  public enddate: Date
-  @column()
-  public endtime: Date
+  @column.date()
+  public startdate: DateTime
 
+  @column()
+  public starttime: any
+
+  @column.date()
+  public enddate: DateTime
+
+  @column()
+  public endtime: any
+
+  @column()
+  public lengthdate: number
+
+  @column()
+  public subject_type: string
+
+  @column()
+  public subject_detail: string
+
+  @column()
+  public destination_hospcode: string
+
+  @column()
+  public destination_other: string
+
+  @column()
+  public official_num: string
+
+  @column()
+  public qty: number
+
+  @column()
+  public appoint_location: string
+
+  @column()
+  public car_member: string
+
+  @column()
+  public status: string
+
+  @column.dateTime()
+  public statusdate: DateTime
+
+  @column()
+  public reason: string
+
+  @column()
+  public length: number
+
+  @column()
+  public driver_id: string
+
+  @column()
+  public startmileage: number
+
+  @column()
+  public endmileage: number
+
+  @column()
+  public priority: string
+
+  @column.dateTime()
+  public lastupdate: DateTime
 }
