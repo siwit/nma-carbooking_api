77ad8d79966389b90831b8d4990595765c034fac
takio1981 <takio1981@gmail.com>
2023-03-29 10:30:19 +0700

    up carmember componant data table

--- app/Controllers/Http/CarMembersControllers.ts
+++ app/Controllers/Http/CarMembersControllers.ts
@@ -4,12 +4,22 @@ import CarMemberLists from 'App/Models/CarMemberModels'
 export default class CarMemberController {
 
     public async getCarMemberList({response}:HttpContextContract){
-        const carMemberList = await CarMemberLists.all()
-        return response.ok(carMemberList)
+       
+        try {
+            const carMemberList = await CarMemberLists.all()          
+            return response.status(200).json({ ok: true, results: carMemberList, code: 200 })
+        } catch (error) {
+            return response.status(500).json({ ok: false, error: 'Internal Server Error', code: 500 })
+        }
     }
     public async getCarMemberById({params,response}:HttpContextContract){
-        const carMemberList = await CarMemberLists.findBy('id',params.id)
-        return response.ok(carMemberList)
+       
+        try {
+            const carMemberList = await CarMemberLists.findBy('id',params.id)           
+            return response.status(200).json({ ok: true, results: carMemberList, code: 200 })
+        } catch (error) {
+            return response.status(500).json({ ok: false, error: 'Internal Server Error', code: 500 })
+        }
     }
 
 
