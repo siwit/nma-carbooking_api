4521d58bac43e1e5eb75e33247e1a0f12d19c2a9
takio1981 <takio1981@gmail.com>
2023-03-29 09:42:00 +0700

    update carmember componant

--- start/routes/CarMemberRoutes.ts
+++ start/routes/CarMemberRoutes.ts
@@ -3,8 +3,8 @@ import CarMembersControllers from 'App/Controllers/Http/CarMembersControllers'
 
 export default function CarMemberRoutes(){
     const carMembersController = new CarMembersControllers()
- Route.get('carbooking/getCarMemberlList',carMembersController.getCarMemberList)
- Route.get('carbooking/getCarMemberById/:id',carMembersController.getCarMemberById)
+ Route.get('carmember/getCarMemberList',carMembersController.getCarMemberList)
+ Route.get('carmember/getCarMemberById/:id',carMembersController.getCarMemberById)
 }
 
 
